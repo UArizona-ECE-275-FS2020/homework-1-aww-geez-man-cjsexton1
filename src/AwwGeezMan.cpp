6879f93ed287b38743f381ad3eff038cbd98f71d
cjsexton1 <70232040+cjsexton1@users.noreply.github.com>
2020-09-26 20:59:32 -0700

    Add files via upload

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,48 +1,109 @@
 /*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
+ _____ ____ _____   ____ _____ ____
+| ____/ ___| ____| |___ \___  | ___|
+|  _|| |   |  _|     __) | / /|___ \
 | |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
+|_____\____|_____| |_____/_/  |____/
+
+ _   _                                         _      _
 | | | | ___  _ __ ___   _____      _____  _ __| | __ / |
 | |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
 |  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
 |_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
 
+
+Aww Geez Man: Interdimensional Mortys.
 One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
 exists a Morty who says everything backwards.
-
 */
 
 // Include the Morty header file
 #include "Morty.hpp"
+#include <iostream>
 
 
-int main (int ac, char** av) {
+
+
+const char* strC137 = "C137";
+const char* strZ286 = "Z286";
+
+int main(int ac, char** av) {
 	// Parse the command line arguments. The program is executed as
 	// ./AwwGeezMan {start} {stop} {dimension}
 	// or 
 	// ./AwwGeezMan {start} {stop} {step} {dimension}
 	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
+		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)"
 			<< std::endl;
 		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
 		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
+
 		return -1;
 	}
-	
+
 	// Parse the command line arguments
-	
-		
-	// Depending on the dimension of the arguments, call the appropriate Morty
-	// function
-	
-	
-	return 0;
+
+	//without steps 
+	if (ac == 4) {
+
+		int startNum = atoi(av[1]);
+
+		int endNum = atoi(av[2]);
+
+		if (*av[3] == *strZ286) {
+
+			Z286::Morty(startNum, endNum);
+
+			return 0;
+
+		}
+		else if (*av[3] == *strC137) {
+
+			C137::Morty(startNum, endNum);
+
+			return 0;
+		}
+		else {
+			std::cout << "ERROR: Unknown dimension!!" << std::endl;
+
+			return -1;
+		}
+	}
+
+
+	//with steps 4 command lines
+	else if (ac == 5) {
+
+
+		int startNum = atoi(av[1]);
+
+		int endNum = atoi(av[2]);
+
+		int stepNum = atoi(av[3]);
+
+
+		if (*av[4] == *strC137) {
+
+			for (startNum; startNum <= endNum; startNum = startNum + stepNum) {
+
+				Z286::Morty(startNum, endNum, stepNum);
+
+				return 0;
+			}
+		}
+		else if (*av[4] == *strC137) {
+
+			C137::Morty(startNum, endNum, stepNum);
+
+			return 0;
+
+		}
+
+		else {
+
+			std::cout << "ERROR: Unknown dimension!!" << std::endl;
+
+			return -1;
+		}
+	}
 }
--- src/Morty.cpp
+++ src/Morty.cpp
@@ -1,10 +1,10 @@
-/* 
- __  __            _                                
-|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \ 
+/*
+ __  __            _
+|  \/  | ___  _ __| |_ _   _        ___ _ __  _ __
+| |\/| |/ _ \| '__| __| | | |      / __| '_ \| '_ \
 | |  | | (_) | |  | |_| |_| |  _  | (__| |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/ 
-                       |___/           |_|   |_|  
+|_|  |_|\___/|_|   \__|\__, | (_)  \___| .__/| .__/
+                       |___/           |_|   |_|
 */
 
 // Include the Morty header file
@@ -14,8 +14,85 @@
 //          Define functions enclosed in Namespace C137 here 
 // =========================================================================== // 
 
+void C137::Morty(int start, int stop) {
 
+	std::cout << "Morty C137 says:" << std::endl;
 
+	for (start; start <= stop; start++) {
+		if (start % 15 == 0) {
+			std::cout << start << ": Aww Geez Man" << std::endl;
+		}
+		else if (start % 5 == 0) {
+			std::cout << start << ": Geez" << std::endl;
+		}
+		else if (start % 3 == 0) {
+			std::cout << start << ": Aww" << std::endl;
+		}
+		else {
+			std::cout << start << ": " << std::endl;
+		}
+	}
+}
+
+void C137::Morty(int start, int stop, int step) {
+	
+	std::cout << "Morty C137 says:" << std::endl;
+	
+	for (start; start <= stop; start = step + start) {
+		if (start % 15 == 0) {
+			std::cout << start << ": Aww Geez Man" << std::endl;
+		}
+		else if (start % 5 == 0) {
+			std::cout << start << ": Geez" << std::endl;
+		}
+		else if (start % 3 == 0) {
+			std::cout << start << ": Aww" << std::endl;
+		}
+		else {
+			std::cout << start << ": " << std::endl;
+		}
+	}
+}
+
+void Z286::Morty(int start, int stop) {
+	
+	std::cout << "Morty C286 says:" << std::endl;
+
+	for (start; start <= stop; start++) {
+		if (start % 15 == 0) {
+			std::cout << start << ": naM zeeG wwA" << std::endl;
+		}
+		else if (start % 5 == 0) {
+			std::cout << start << ": zeeG" << std::endl;
+		}
+		else if (start % 3 == 0) {
+			std::cout << start << ": wwA" << std::endl;
+		}
+		else {
+			std::cout << start << ": " << std::endl;
+		}
+	}
+}
+
+void Z286::Morty(int start, int stop, int step) {
+	
+	std::cout << "Morty C286 says:" << std::endl;
+
+	for (start; start <= stop; start = step + start) {
+		if (start % 15 == 0) {
+			std::cout << start << ": naM zeeG wwA" << std::endl;
+		}
+		else if (start % 5 == 0) {
+			std::cout << start << ": zeeG" << std::endl;
+		}
+		else if (start % 3 == 0) {
+			std::cout << start << ": wwA" << std::endl;
+		}
+		else {
+			std::cout << start << ": " << std::endl;
+		}
+	}
+}
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
